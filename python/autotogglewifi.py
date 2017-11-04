3d0a6643b589de3637165fdba2cfa9985d19c05f
Felix5721 <f.judge42@gmail.com>
2017-11-04 15:57:36 +0100

    added functions to update the status icon in awesome

--- python/autotogglewifi.py
+++ python/autotogglewifi.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import subprocess
 import re
+import socket
 from pydbus import SystemBus
 from gi.repository import GLib
 
@@ -18,17 +19,21 @@ class NetworkattachListener:
 		self.wired_iface = self.system_bus.get(NETWORKD, WIRED+str(self.getWiredNum('enp0s25')));
 		self.wireless_iface = self.system_bus.get(NETWORKD, WIRED+str(self.getWiredNum('wlp4s0')));
 		self.wireless_srv = self.system_bus.get(SYSTEMD, self.sysmanager.GetUnit(WIRELESS_SERVICE))
+		self.wireless_status = False
+		self.eth_status = False
 
 		self.wired_iface.PropertiesChanged.connect(self.worker_wlan)
 		self.wireless_iface.PropertiesChanged.connect(self.worker_vpn)
 		if self.isRoutable(self.wired_iface.Get(NETWORKD + ".Link", "OperationalState")):
+			self.eth_status = True
 			self.WirelessOff()
 		else:
 			self.WirelessOn()
+		self.updateIcon()
 
 	def getWiredNum(self, iface_name):
-    		#I know this isn't a nice way to do it, but I didn't find any other documentation on how to get his down
-    		#If you do now how to get this information properly, please let me know
+		#I know this isn't a nice way to do it, but I didn't find any other documentation on how to get his down
+		#If you do now how to get this information properly, please let me know
 		linkn = subprocess.run(['networkctl', 'list', iface_name], stdout=subprocess.PIPE).stdout.decode("utf-8")
 		linkn = re.search("(?<=\n).*(?= "+iface_name+")", linkn).group(0)
 		return int(linkn)
@@ -43,15 +48,37 @@ class NetworkattachListener:
 	def worker_vpn(self, interface, changed, inv):
 		if "OperationalState" in changed.keys():
 			if self.isRoutable(changed["OperationalState"]):
+				self.wireless_status = True
 				self.sysmanager.StopUnit(VPN, "fail")
 				self.sysmanager.StartUnit(VPN, "replace")
+			else:
+				self.wireless_status = False
+			self.updateIcon()
 
 	def worker_wlan(self, interface, changed, inv):
 		if "OperationalState" in changed.keys():
 			if self.isRoutable(changed["OperationalState"]):
+				self.eth_status = True
 				self.WirelessOff()
 			else:
+				self.eth_status = False
 				self.WirelessOn()
+			self.updateIcon()
+
+	def updateIcon(self):
+		if self.eth_status or self.wireless_status:
+			if self.eth_status:
+				self.setIcon("ethernet")
+			else:
+				self.setIcon("wireless")
+		else:
+			self.setIcon("offline")
+
+	def setIcon(self, state):
+		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		sock.connect(("localhost", 4224))
+		sock.send(state.encode())
+		sock.close()
 
 	def getWirelessState(self):
 		return self.wireless_srv.Get(SYSTEMD + ".Unit", "ActiveState")
