3da3e85427a3eee4bd31a8e0eb384f771ba66503
Felix5721 <f.judge42@gmail.com>
2018-04-26 13:04:04 +0200

    fixed autoWifi script to check for eduroam network and choose vpn programm accordingly

--- python/autotogglewifi.py
+++ python/autotogglewifi.py
@@ -2,14 +2,17 @@
 import subprocess
 import re
 import socket
+import time
 from pydbus import SystemBus
 from gi.repository import GLib
 
 WIRELESS_SERVICE = "network-wireless@wlp4s0.service"
 WIRED = "/org/freedesktop/network1/link/_3"
-VPN = "openvpn-client@dijkstra.service"
+VPN = "wireguard@vpn.service"
+FALLBACK_VPN = "openvpn-client@dijkstra.service"
 SYSTEMD = "org.freedesktop.systemd1"
 NETWORKD = "org.freedesktop.network1"
+WPA_INTERFACE = "fi.w1.wpa_supplicant1"
 
 class NetworkattachListener:
 	def __init__(self):
@@ -19,8 +22,10 @@ class NetworkattachListener:
 		self.wired_iface = self.system_bus.get(NETWORKD, WIRED+str(self.getWiredNum('enp0s25')));
 		self.wireless_iface = self.system_bus.get(NETWORKD, WIRED+str(self.getWiredNum('wlp4s0')));
 		self.wireless_srv = self.system_bus.get(SYSTEMD, self.sysmanager.GetUnit(WIRELESS_SERVICE))
+		self.wpa_iface = self.system_bus.get(WPA_INTERFACE, "/fi/w1/wpa_supplicant1/Interfaces/0")
 		self.wireless_status = False
 		self.eth_status = False
+
 
 		self.wired_iface.PropertiesChanged.connect(self.worker_wlan)
 		self.wireless_iface.PropertiesChanged.connect(self.worker_vpn)
@@ -29,7 +34,8 @@ class NetworkattachListener:
 			self.WirelessOff()
 		else:
 			self.WirelessOn()
-		self.updateIcon()
+		if self.isRoutable(self.wireless_iface.Get(NETWORKD + ".Link", "OperationalState")):
+			self.wireless_status = True
 
 	def getWiredNum(self, iface_name):
 		#I know this isn't a nice way to do it, but I didn't find any other documentation on how to get his down
@@ -49,11 +55,20 @@ class NetworkattachListener:
 		if "OperationalState" in changed.keys():
 			if self.isRoutable(changed["OperationalState"]):
 				self.wireless_status = True
-				self.sysmanager.StopUnit(VPN, "fail")
-				self.sysmanager.StartUnit(VPN, "replace")
+				ssid = self.getCurrentNetwork()
+				if ssid == "eduroam":
+					print("start openvpn")
+					self.sysmanager.StopUnit(FALLBACK_VPN, "fail")
+					self.sysmanager.StopUnit(VPN, "fail")
+					self.sysmanager.StartUnit(FALLBACK_VPN, "replace")
+				else:
+					print("start wireguard")
+					self.sysmanager.StopUnit(FALLBACK_VPN, "fail")
+					self.sysmanager.StopUnit(VPN, "fail")
+					self.sysmanager.StartUnit(VPN, "replace")
+
 			else:
 				self.wireless_status = False
-			self.updateIcon()
 
 	def worker_wlan(self, interface, changed, inv):
 		if "OperationalState" in changed.keys():
@@ -63,22 +78,6 @@ class NetworkattachListener:
 			else:
 				self.eth_status = False
 				self.WirelessOn()
-			self.updateIcon()
-
-	def updateIcon(self):
-		if self.eth_status or self.wireless_status:
-			if self.eth_status:
-				self.setIcon("ethernet")
-			else:
-				self.setIcon("wireless")
-		else:
-			self.setIcon("offline")
-
-	def setIcon(self, state):
-		sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		sock.connect(("localhost", 4224))
-		sock.send(state.encode())
-		sock.close()
 
 	def getWirelessState(self):
 		return self.wireless_srv.Get(SYSTEMD + ".Unit", "ActiveState")
@@ -91,14 +90,22 @@ class NetworkattachListener:
 			else:
 				self.sysmanager.StopUnit(WIRELESS_SERVICE, "fail")
 				self.sysmanager.StopUnit(VPN, "fail")
+				self.sysmanager.StopUnit(FALLBACK_VPN, "fail")
 				self.sysmanager.StartUnit(VPN, "replace")
 
 	def WirelessOn(self):
 		print("Turn Wifi on")
 		self.setWireless(True)
+
 	def WirelessOff(self):
 		print("Turn Wifi off")
 		self.setWireless(False)
+
+	def getCurrentNetwork(self):
+		currentNetwork = self.wpa_iface.Get(WPA_INTERFACE + ".Interface", "CurrentNetwork")
+		currentWPA = self.system_bus.get(WPA_INTERFACE, currentNetwork)
+		ssid = currentWPA.Get(WPA_INTERFACE + ".Network", "Properties")["ssid"]
+		return ssid[1:-1]
 
 def main():
 	loop = GLib.MainLoop()
